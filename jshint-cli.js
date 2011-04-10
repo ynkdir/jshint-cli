a3f614d715e8d18032466f84c8344da61c50cbc5
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2011-04-10 14:03:11 +0900

    upstream update

--- jshint-cli.js
+++ jshint-cli.js
@@ -5,35 +5,32 @@ function usage() {
   print('  --help');
   print('  --asi        true, if automatic semicolon insertion should be tolerated');
   print('  --bitwise    true, if bitwise operators should not be allowed');
-  print('  --browser    true, if the standard browser globals should be predefined');
   print('  --boss       true, if advanced usage of assignments and == should be allowed');
-  print('  --cap        true, if upper case HTML should be allowed');
+  print('  --browser    true, if the standard browser globals should be predefined');
   print('  --couch      true, if CouchDB globals should be predefined');
-  print('  --css        true, if CSS workarounds should be tolerated');
   print('  --curly      true, if curly braces around blocks should be required (even in if/for/while)');
   print('  --debug      true, if debugger statements should be allowed');
   print('  --devel      true, if logging should be allowed (console, alert, etc.)');
   print('  --eqeqeq     true, if === should be required');
+  print('  --eqnull     true, if == null comparisons should be tolerated');
   print('  --es5        true, if ES5 syntax should be allowed');
   print('  --evil       true, if eval should be allowed');
   print('  --expr       true, if ExpressionStatement should be allowed as Programs');
   print('  --forin      true, if for in statements need not filter');
+  print('  --globalstrict true, if global "use strict"; should be allowed (also enables \'strict\'');
   print('  --fragment   true, if HTML fragments should be allowed');
   print('  --immed      true, if immediate invocations must be wrapped in parens');
   print('  --jquery     true, if jQuery globals should be predefined');
-  print('  --indent     the indentation factor');
   print('  --latedef    true, if the use before definition should not be tolerated');
   print('  --laxbreak   true, if line breaks should not be checked');
   print('  --loopfunc   true, if functions should be allowed to be defined within loops');
-  print('  --maxerr     the maximum number of errors to allow');
-  print('  --maxlen     the maximum length of a source line');
+  print('  --mootools   true, if MooTools globals should be predefined');
   print('  --newcap     true, if constructor names must be capitalized');
   print('  --noarg      true, if arguments.caller and arguments.callee should be disallowed');
   print('  --node       true, if Node.js globals should be predefined');
   print('  --noempty    true, if empty blocks should be disallowed');
   print('  --nonew      true, if using `new` for side-effects should be disallowed');
   print('  --nomen      true, if names should be checked');
-  print('  --on         true, if HTML event handlers should be allowed');
   print('  --onevar     true, if only one var statement per function should be allowed');
   print('  --passfail   true, if the scan should stop on first error');
   print('  --plusplus   true, if increment/decrement should not be allowed');
@@ -45,6 +42,9 @@ function usage() {
   print('  --strict     true, require the "use strict"; pragma');
   print('  --sub        true, if all forms of subscript notation are tolerated');
   print('  --white      true, if strict whitespace rules apply');
+  print('  --indent=N   the indentation factor');
+  print('  --maxerr=N   the maximum number of errors to allow');
+  print('  --maxlen=N   the maximum length of a source line');
   exit(0);
 }
 
@@ -61,14 +61,10 @@ function parse_arguments(args) {
       res.option.asi = true;
     } else if (args[i] === '--bitwise') {
       res.option.bitwise = true;
-    } else if (args[i] === '--browser') {
-      res.option.browser = true;
     } else if (args[i] === '--boss') {
       res.option.boss = true;
-    } else if (args[i] === '--cap') {
-      res.option.cap = true;
-    } else if (args[i] === '--css') {
-      res.option.css = true;
+    } else if (args[i] === '--browser') {
+      res.option.browser = true;
     } else if (args[i] === '--couch') {
       res.option.couch = true;
     } else if (args[i] === '--debug') {
@@ -77,6 +73,8 @@ function parse_arguments(args) {
       res.option.devel = true;
     } else if (args[i] === '--eqeqeq') {
       res.option.eqeqeq = true;
+    } else if (args[i] === '--eqnull') {
+      res.option.eqnull = true;
     } else if (args[i] === '--es5') {
       res.option.devel = true;
     } else if (args[i] === '--evil') {
@@ -85,16 +83,12 @@ function parse_arguments(args) {
       res.option.expr = true;
     } else if (args[i] === '--forin') {
       res.option.forin = true;
+    } else if (args[i] === '--globalstrict') {
+      res.option.globalstrict = true;
     } else if (args[i] === '--fragment') {
       res.option.fragment = true;
     } else if (args[i] === '--immed') {
       res.option.immed = true;
-    } else if (args[i].match(/^--indent(?:=(.+))?/)) {
-      if (RegExp.$1 !== '') {
-        res.option.indent = parseInt(RegExp.$1, 10);
-      } else {
-        res.option.indent = parseInt(process.argv[++i], 10);
-      }
     } else if (args[i] === '--jquery') {
       res.option.jquery = true;
     } else if (args[i] === '--latedef') {
@@ -103,18 +97,8 @@ function parse_arguments(args) {
       res.option.laxbreak = true;
     } else if (args[i] === '--loopfunc') {
       res.option.loopfunc = true;
-    } else if (args[i].match(/^--maxerr(?:=(.+))?/)) {
-      if (RegExp.$1 !== '') {
-        res.option.maxerr = parseInt(RegExp.$1, 10);
-      } else {
-        res.option.maxerr = parseInt(process.argv[++i], 10);
-      }
-    } else if (args[i].match(/^--maxlen(?:=(.+))?/)) {
-      if (RegExp.$1 !== '') {
-        res.option.maxlen = parseInt(RegExp.$1, 10);
-      } else {
-        res.option.maxlen = parseInt(process.argv[++i], 10);
-      }
+    } else if (args[i] === '--mootools') {
+      res.option.mootools = true;
     } else if (args[i] === '--newcap') {
       res.option.newcap = true;
     } else if (args[i] === '--noarg') {
@@ -127,8 +111,6 @@ function parse_arguments(args) {
       res.option.nonew = true;
     } else if (args[i] === '--nomen') {
       res.option.nomen = true;
-    } else if (args[i] === '--on') {
-      res.option.on = true;
     } else if (args[i] === '--onevar') {
       res.option.onevar = true;
     } else if (args[i] === '--passfail') {
@@ -151,6 +133,24 @@ function parse_arguments(args) {
       res.option.sub = true;
     } else if (args[i] === '--white') {
       res.option.white = true;
+    } else if (args[i].match(/^--indent(?:=(.+))?/)) {
+      if (RegExp.$1 !== '') {
+        res.option.indent = parseInt(RegExp.$1, 10);
+      } else {
+        res.option.indent = parseInt(process.argv[++i], 10);
+      }
+    } else if (args[i].match(/^--maxerr(?:=(.+))?/)) {
+      if (RegExp.$1 !== '') {
+        res.option.maxerr = parseInt(RegExp.$1, 10);
+      } else {
+        res.option.maxerr = parseInt(process.argv[++i], 10);
+      }
+    } else if (args[i].match(/^--maxlen(?:=(.+))?/)) {
+      if (RegExp.$1 !== '') {
+        res.option.maxlen = parseInt(RegExp.$1, 10);
+      } else {
+        res.option.maxlen = parseInt(process.argv[++i], 10);
+      }
     } else if (args[i].match(/^-/)) {
       print('unknown option: ' + args[i]);
       exit(1);
