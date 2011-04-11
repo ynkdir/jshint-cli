e1ec3fd53666b5558466c1b9141b2d3dd53fb6f7
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2011-04-12 08:08:15 +0900

    Added optspec.js

--- Makefile
+++ Makefile
@@ -1,21 +1,22 @@
 
+wget=wget --no-check-certificate
+
 all: bin/jshint bin/jshint.bat
 
-bin/jshint: jshint.js jshint-node.js jshint-cli.js
-	echo "#!/usr/bin/env node" > bin/jshint
-	cat jshint.js jshint-node.js jshint-cli.js >> bin/jshint
+bin/jshint: jshint.js jshint-node.js optspec.js jshint-cli.js
+	echo "#!/usr/bin/env node" > $@
+	cat $^ >> $@
 
-bin/jshint.bat: jshint.js jshint-wsh.js jshint-cli.js
+bin/jshint.bat: jshint.js jshint-wsh.js optspec.js jshint-cli.js
 	echo '@set @junk=1 /*' > $@
 	echo '@echo off' >> $@
 	echo 'cscript //nologo //E:jscript "%~dpn0.bat" %*' >> $@
 	echo 'goto :eof' >> $@
 	echo '*/' >> $@
-	cat jshint.js jshint-wsh.js jshint-cli.js >> $@
+	cat $^ >> $@
 	# unix2dos
-	sed 's/$$/\r/' $@ > __tmp
-	mv __tmp $@
+	sed 's/$$/\r/' $@ > __tmp && mv __tmp $@
 
 jshint.js:
-	wget --no-check-certificate https://github.com/jshint/jshint/raw/master/jshint.js
+	$(wget) https://github.com/jshint/jshint/raw/master/jshint.js
 
--- jshint-cli.js
+++ jshint-cli.js
@@ -1,178 +1,65 @@
 
-function usage() {
-  print('jshint [options] file');
-  print('options:');
-  print('  --help');
-  print('  --asi        true, if automatic semicolon insertion should be tolerated');
-  print('  --bitwise    true, if bitwise operators should not be allowed');
-  print('  --boss       true, if advanced usage of assignments and == should be allowed');
-  print('  --browser    true, if the standard browser globals should be predefined');
-  print('  --couch      true, if CouchDB globals should be predefined');
-  print('  --curly      true, if curly braces around blocks should be required (even in if/for/while)');
-  print('  --debug      true, if debugger statements should be allowed');
-  print('  --devel      true, if logging should be allowed (console, alert, etc.)');
-  print('  --eqeqeq     true, if === should be required');
-  print('  --eqnull     true, if == null comparisons should be tolerated');
-  print('  --es5        true, if ES5 syntax should be allowed');
-  print('  --evil       true, if eval should be allowed');
-  print('  --expr       true, if ExpressionStatement should be allowed as Programs');
-  print('  --forin      true, if for in statements need not filter');
-  print('  --globalstrict true, if global "use strict"; should be allowed (also enables \'strict\'');
-  print('  --fragment   true, if HTML fragments should be allowed');
-  print('  --immed      true, if immediate invocations must be wrapped in parens');
-  print('  --jquery     true, if jQuery globals should be predefined');
-  print('  --latedef    true, if the use before definition should not be tolerated');
-  print('  --laxbreak   true, if line breaks should not be checked');
-  print('  --loopfunc   true, if functions should be allowed to be defined within loops');
-  print('  --mootools   true, if MooTools globals should be predefined');
-  print('  --newcap     true, if constructor names must be capitalized');
-  print('  --noarg      true, if arguments.caller and arguments.callee should be disallowed');
-  print('  --node       true, if Node.js globals should be predefined');
-  print('  --noempty    true, if empty blocks should be disallowed');
-  print('  --nonew      true, if using `new` for side-effects should be disallowed');
-  print('  --nomen      true, if names should be checked');
-  print('  --onevar     true, if only one var statement per function should be allowed');
-  print('  --passfail   true, if the scan should stop on first error');
-  print('  --plusplus   true, if increment/decrement should not be allowed');
-  print('  --prototypejs true, if Prototype and Scriptaculous globals shoudl be predefined');
-  print('  --regexp     true, if the . should not be allowed in regexp literals');
-  print('  --rhino      true, if the Rhino environment globals should be predefined');
-  print('  --undef      true, if variables should be declared before used');
-  print('  --shadow     true, if variable shadowing should be tolerated');
-  print('  --strict     true, require the "use strict"; pragma');
-  print('  --sub        true, if all forms of subscript notation are tolerated');
-  print('  --supernew   true, if `new function () { ... };` and `new Object;` should be tolerated');
-  print('  --white      true, if strict whitespace rules apply');
-  print('  --indent=N   the indentation factor');
-  print('  --maxerr=N   the maximum number of errors to allow');
-  print('  --maxlen=N   the maximum length of a source line');
-  exit(0);
-}
-
-function parse_arguments(args) {
-  var res = {
-    args: [],
-    option: {}
-  };
-  var i;
-  for (i = 0; i < args.length; ++i) {
-    if (args[i] === '-h' || args[i] === '--help') {
-      usage();
-    } else if (args[i] === '--asi') {
-      res.option.asi = true;
-    } else if (args[i] === '--bitwise') {
-      res.option.bitwise = true;
-    } else if (args[i] === '--boss') {
-      res.option.boss = true;
-    } else if (args[i] === '--browser') {
-      res.option.browser = true;
-    } else if (args[i] === '--couch') {
-      res.option.couch = true;
-    } else if (args[i] === '--debug') {
-      res.option.debug = true;
-    } else if (args[i] === '--devel') {
-      res.option.devel = true;
-    } else if (args[i] === '--eqeqeq') {
-      res.option.eqeqeq = true;
-    } else if (args[i] === '--eqnull') {
-      res.option.eqnull = true;
-    } else if (args[i] === '--es5') {
-      res.option.devel = true;
-    } else if (args[i] === '--evil') {
-      res.option.evil = true;
-    } else if (args[i] === '--expr') {
-      res.option.expr = true;
-    } else if (args[i] === '--forin') {
-      res.option.forin = true;
-    } else if (args[i] === '--globalstrict') {
-      res.option.globalstrict = true;
-    } else if (args[i] === '--fragment') {
-      res.option.fragment = true;
-    } else if (args[i] === '--immed') {
-      res.option.immed = true;
-    } else if (args[i] === '--jquery') {
-      res.option.jquery = true;
-    } else if (args[i] === '--latedef') {
-      res.option.latedef = true;
-    } else if (args[i] === '--laxbreak') {
-      res.option.laxbreak = true;
-    } else if (args[i] === '--loopfunc') {
-      res.option.loopfunc = true;
-    } else if (args[i] === '--mootools') {
-      res.option.mootools = true;
-    } else if (args[i] === '--newcap') {
-      res.option.newcap = true;
-    } else if (args[i] === '--noarg') {
-      res.option.noarg = true;
-    } else if (args[i] === '--node') {
-      res.option.node = true;
-    } else if (args[i] === '--noempty') {
-      res.option.noempty = true;
-    } else if (args[i] === '--nonew') {
-      res.option.nonew = true;
-    } else if (args[i] === '--nomen') {
-      res.option.nomen = true;
-    } else if (args[i] === '--onevar') {
-      res.option.onevar = true;
-    } else if (args[i] === '--passfail') {
-      res.option.passfail = true;
-    } else if (args[i] === '--plusplus') {
-      res.option.plusplus = true;
-    } else if (args[i] === '--prototypejs') {
-      res.option.prototypejs = true;
-    } else if (args[i] === '--regexp') {
-      res.option.regexp = true;
-    } else if (args[i] === '--rhino') {
-      res.option.rhino = true;
-    } else if (args[i] === '--undef') {
-      res.option.undef = true;
-    } else if (args[i] === '--shadow') {
-      res.option.shadow = true;
-    } else if (args[i] === '--strict') {
-      res.option.strict = true;
-    } else if (args[i] === '--sub') {
-      res.option.sub = true;
-    } else if (args[i] === '--supernew') {
-      res.option.supernew = true;
-    } else if (args[i] === '--white') {
-      res.option.white = true;
-    } else if (args[i].match(/^--indent(?:=(.+))?/)) {
-      if (RegExp.$1 !== '') {
-        res.option.indent = parseInt(RegExp.$1, 10);
-      } else {
-        res.option.indent = parseInt(process.argv[++i], 10);
-      }
-    } else if (args[i].match(/^--maxerr(?:=(.+))?/)) {
-      if (RegExp.$1 !== '') {
-        res.option.maxerr = parseInt(RegExp.$1, 10);
-      } else {
-        res.option.maxerr = parseInt(process.argv[++i], 10);
-      }
-    } else if (args[i].match(/^--maxlen(?:=(.+))?/)) {
-      if (RegExp.$1 !== '') {
-        res.option.maxlen = parseInt(RegExp.$1, 10);
-      } else {
-        res.option.maxlen = parseInt(process.argv[++i], 10);
-      }
-    } else if (args[i].match(/^-/)) {
-      print('unknown option: ' + args[i]);
-      exit(1);
-    } else {
-      res.args.push(args[i]);
-    }
-  }
-  return res;
-}
+var option_parser = new optspec.OptionParser('jshint [options] file');
+option_parser.add_option('  -h, --help     show this help message and exit');
+option_parser.add_option('  --asi          true, if automatic semicolon insertion should be tolerated');
+option_parser.add_option('  --bitwise      true, if bitwise operators should not be allowed');
+option_parser.add_option('  --boss         true, if advanced usage of assignments and == should be allowed');
+option_parser.add_option('  --browser      true, if the standard browser globals should be predefined');
+option_parser.add_option('  --couch        true, if CouchDB globals should be predefined');
+option_parser.add_option('  --curly        true, if curly braces around blocks should be required (even in if/for/while)');
+option_parser.add_option('  --debug        true, if debugger statements should be allowed');
+option_parser.add_option('  --devel        true, if logging should be allowed (console, alert, etc.)');
+option_parser.add_option('  --eqeqeq       true, if === should be required');
+option_parser.add_option('  --eqnull       true, if == null comparisons should be tolerated');
+option_parser.add_option('  --es5          true, if ES5 syntax should be allowed');
+option_parser.add_option('  --evil         true, if eval should be allowed');
+option_parser.add_option('  --expr         true, if ExpressionStatement should be allowed as Programs');
+option_parser.add_option('  --forin        true, if for in statements need not filter');
+option_parser.add_option('  --globalstrict  true, if global "use strict"; should be allowed (also enables \'strict\'');
+option_parser.add_option('  --fragment     true, if HTML fragments should be allowed');
+option_parser.add_option('  --immed        true, if immediate invocations must be wrapped in parens');
+option_parser.add_option('  --indent=NUMBER  the indentation factor');
+option_parser.add_option('  --jquery       true, if jQuery globals should be predefined');
+option_parser.add_option('  --latedef      true, if the use before definition should not be tolerated');
+option_parser.add_option('  --laxbreak     true, if line breaks should not be checked');
+option_parser.add_option('  --loopfunc     true, if functions should be allowed to be defined within loops');
+option_parser.add_option('  --maxerr=NUMBER  the maximum number of errors to allow');
+option_parser.add_option('  --maxlen=NUMBER  the maximum length of a source line');
+option_parser.add_option('  --mootools     true, if MooTools globals should be predefined');
+option_parser.add_option('  --newcap       true, if constructor names must be capitalized');
+option_parser.add_option('  --noarg        true, if arguments.caller and arguments.callee should be disallowed');
+option_parser.add_option('  --node         true, if Node.js globals should be predefined');
+option_parser.add_option('  --noempty      true, if empty blocks should be disallowed');
+option_parser.add_option('  --nonew        true, if using `new` for side-effects should be disallowed');
+option_parser.add_option('  --nomen        true, if names should be checked');
+option_parser.add_option('  --onevar       true, if only one var statement per function should be allowed');
+option_parser.add_option('  --passfail     true, if the scan should stop on first error');
+option_parser.add_option('  --plusplus     true, if increment/decrement should not be allowed');
+option_parser.add_option('  --prototypejs  true, if Prototype and Scriptaculous globals shoudl be predefined');
+option_parser.add_option('  --regexp       true, if the . should not be allowed in regexp literals');
+option_parser.add_option('  --rhino        true, if the Rhino environment globals should be predefined');
+option_parser.add_option('  --undef        true, if variables should be declared before used');
+option_parser.add_option('  --shadow       true, if variable shadowing should be tolerated');
+option_parser.add_option('  --strict       true, require the "use strict"; pragma');
+option_parser.add_option('  --sub          true, if all forms of subscript notation are tolerated');
+option_parser.add_option('  --supernew     true, if `new function () { ... };` and `new Object;` should be tolerated');
+option_parser.add_option('  --white        true, if strict whitespace rules apply');
 
 function main() {
   var args, filename, data, noerror, i;
-  args = parse_arguments(getargs());
-  if (args.args.length === 0) {
-    usage();
+  try {
+    args = option_parser.parse_args(getargs());
+  } catch (ex) {
+    print(ex.message);
+    exit(1);
+  }
+  if (args.opts.help || args.args.length === 0) {
+    print(option_parser.format_help());
+    exit(0);
   }
   filename = args.args[0];
   data = readfile(filename, 'utf-8');
-  noerror = JSHINT(data, args.option);
+  noerror = JSHINT(data, args.opts);
   if (!noerror) {
     for (i = 0; i < JSHINT.errors.length; ++i) {
       var e = JSHINT.errors[i];
