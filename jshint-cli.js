c9d112cc0ba4ce8db6ca04c2a4a2f540cc221a0a
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2011-05-07 01:05:26 +0900

    Fixed message

--- jshint-cli.js
+++ jshint-cli.js
@@ -62,7 +62,7 @@ function geterrors() {
         errors.push({
           line: e.line,
           col: e.character,
-          message: e.reason + ' : ' + e.evidence
+          message: e.reason
         });
       }
     }
